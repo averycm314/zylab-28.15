0bb24b080c45249e2e1fe7dfc15baa38026d2028
averycm314 <152202601+averycm314@users.noreply.github.com>
2023-12-01 10:10:37 -0600

    Update main.cpp
    updated to create variables and begin function definitions -AM

--- main.cpp
+++ main.cpp
@@ -3,8 +3,49 @@ using namespace std;
 
 int main() { 
 
-  int num; 
-  cout << num;
-  
-return 0; 
+  void PrintMenu(const string playlistTitle) {
+   cout << "a - Add song" << endl; 
+   cout << "b - Remove song" << endl; 
+   cout << "c - Change position of song" << endl;
+   cout << "s - Output songs by specific artist" << endl; 
+   cout << "t - Output total time of playlist (in seconds)" << endl; 
+   cout << "o - Output full playlist" << endl; 
+   cout << "q - Quit"; 
+}
+
+PlaylistNode* ExecuteMenu(char option, string playlistTitle, PlaylistNode* headNode) {
+   getline(cin, option); 
+   if ( option == 'a' ) { 
+     //add song 
+   } 
+   if ( option == 'b') { 
+     // remove song 
+   } 
+   if (option == 'c') { 
+     // change position - can skip 
+      } 
+   if (option == 's') { 
+     // OutputByArtist
+   } 
+   if (option == 't') {
+     // Output song time
+   } 
+   if (option == 'o') { 
+     // Output full playlist 
+   } 
+   if (option == 'q' ) { 
+     break; 
+     // quit 
+   } 
+   
+}
+
+int main() {
+   string playlistTitle; 
+   cout << "Enter the playlist's title:" << endl; 
+   getline(cin, playlistTitle);   
+  PrintMenu(playlistTitle); 
+   
+   return 0;
+}
 }
